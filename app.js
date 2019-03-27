b5a515153f4187c5660ca5148ed46a1029de72a6
camgelozzi <cangelozzi@gmail.com>
2019-03-27 13:10:20 -0400

    socket connect and vue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+
+var io = require('socket.io')();
 
 const port = process.env.PORT || 3000;
 
@@ -14,6 +14,33 @@ app.get('/', (req, res) => {
 
 });
 
-http.listen(port, () => {
+// const server = http.listen(port, () => {
+//   console.log(`app is running on port ${port}`);
+// });
+
+const server = app.listen(port, ()=> {
   console.log(`app is running on port ${port}`);
+});
+
+//! socket.io chat app stuff to follow note. socket is client
+
+io.attach(server);
+
+io.on('connection', function (socket) {
+  console.log('a user has connected');
+
+  // sending back msg to connecting tool
+  socket.emit('connected', {sID: `${socket.id}`, message: 'new connection'});
+
+  // listen for incoming message form anyone connected to the app
+  socket.on('chat message', function(msg) {
+    console.log('message', msg, 'socket', socket.id);
+
+    // send message to everyone connected to the app
+    io.emit('chat message', { id: `${socket.id}`, message: msg });
+  });
+
+  socket.on('disconnect', function(){
+    console.log('a user has disconnected');
+  });
 });
